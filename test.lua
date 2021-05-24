39a0762113b22aac7cbac653d0d3eda756629232
Benjamin Hogl <9270901+EEP-Benny@users.noreply.github.com>
2021-05-24 11:39:25 +0200

    Implement serialization

--- test.lua
+++ test.lua
@@ -160,6 +160,102 @@ do
 end
 --#endregion toNumberOfSeconds()
 
+--#region serialize()
+test(
+  'serialize.should correctly serialize nil, booleans and numbers',
+  function()
+    local serialize = getRunAfterWithPrivate().private.serialize
+    lu.assertEquals(serialize(nil), 'nil')
+    lu.assertEquals(serialize(true), 'true')
+    lu.assertEquals(serialize(false), 'false')
+    lu.assertEquals(serialize(1), '1')
+    lu.assertEquals(serialize(-1.5), '-1.5')
+  end
+)
+
+test(
+  'serialize.should correctly serialize strings',
+  function()
+    local serialize = getRunAfterWithPrivate().private.serialize
+    lu.assertEquals(serialize('simple string'), '"simple string"')
+    lu.assertEquals(serialize('new\nline'), '"new\\nline"')
+    lu.assertEquals(serialize("\"double\" and 'single' quotes"), "\"\\\"double\\\" and 'single' quotes\"")
+  end
+)
+
+test(
+  'serialize.should serialize tables with numeric indices in array form',
+  function()
+    local serialize = getRunAfterWithPrivate().private.serialize
+    lu.assertEquals(serialize({1, 2, 3, 4, 5}), '{1,2,3,4,5}')
+    lu.assertEquals(serialize({'a', 'b', 'c'}), '{"a","b","c"}')
+  end
+)
+
+test(
+  'serialize.should serialize tables with non-continuous numeric indices',
+  function()
+    local serialize = getRunAfterWithPrivate().private.serialize
+    lu.assertEquals(serialize({[1] = 1, [2] = 2, [4] = 4}), '{1,2,[4]=4}')
+  end
+)
+
+test(
+  'serialize.should serialize tables with simple string indices using shorthand notation',
+  function()
+    local serialize = getRunAfterWithPrivate().private.serialize
+    lu.assertEquals(serialize({a = 1}), '{a=1}')
+    lu.assertEquals(serialize({['B_1'] = 1}), '{B_1=1}')
+  end
+)
+
+test(
+  'serialize.should serialize tables with non-identifier-indices using bracket notation',
+  function()
+    local serialize = getRunAfterWithPrivate().private.serialize
+    lu.assertEquals(serialize({['nil'] = 1}), '{["nil"]=1}')
+    lu.assertEquals(serialize({['with spaces'] = 2}), '{["with spaces"]=2}')
+  end
+)
+
+test(
+  'serialize.should serialize tables with numeric indices before string indices',
+  function()
+    local serialize = getRunAfterWithPrivate().private.serialize
+    lu.assertEquals(serialize({1, a = 2}), '{1,a=2}')
+  end
+)
+
+test(
+  "serialize.should serialize tables with multiple references to the same table as long as they don't recurse",
+  function()
+    local serialize = getRunAfterWithPrivate().private.serialize
+    local tbl1 = {1}
+    local tbl2 = {tbl1, tbl1, [tbl1] = tbl1}
+    lu.assertEquals(serialize(tbl2), '{{1},{1},[{1}]={1}}')
+  end
+)
+
+test(
+  'serialize.should throw an error for recursive tables',
+  function()
+    local serialize = getRunAfterWithPrivate().private.serialize
+    local tbl = {}
+    tbl.tbl = tbl
+    lu.assertErrorMsgContentEquals('cannot serialize recursive tables', serialize, tbl)
+  end
+)
+
+test(
+  'serialize.should throw an error for unsupported types',
+  function()
+    local serialize = getRunAfterWithPrivate().private.serialize
+    local expectedMessage = 'serializing values of type function is not supported'
+    lu.assertErrorMsgContentEquals(expectedMessage, serialize, functionReturning(nil))
+  end
+)
+--#endregion serialize()
+
 --#region getCurrentTime()
 test(
   'getCurrentTime.should return the current time for a given axis position',
